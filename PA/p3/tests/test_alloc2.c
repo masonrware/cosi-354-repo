618ef94a29a0150d859e1de86454f694a56e1a6f
masonrware <86129546+masonrware@users.noreply.github.com>
2023-03-04 18:13:19 -0600

    progress on p3 mostly

---
+++ PA/p3/tests/test_alloc2.c
@@ -0,0 +1,16 @@
+// a few allocations in multiples of 4 bytes
+#include <assert.h>
+#include <stdlib.h>
+#include "p3Heap.h"
+
+int main() {
+   assert(init_heap(4096) == 0);
+   assert(balloc(4) != NULL);
+   assert(balloc(8) != NULL);
+   assert(balloc(16) != NULL);
+   assert(balloc(24) != NULL);
+
+   disp_heap();
+
+   exit(0);
+}
---
+++ PA/p3/tests/test_alloc2_nospace.c
@@ -0,0 +1,14 @@
+// second allocation is too big to fit
+#include <assert.h>
+#include <stdlib.h>
+#include "p3Heap.h"
+
+int main() {
+   assert(init_heap(4096) == 0);
+   assert(balloc(2048) != NULL);
+   assert(balloc(2047) == NULL);
+
+	disp_heap();
+
+   exit(0);
+}
